387c120888890889259c4fec8b422877d5b60348
GuptaVardan <guptavardan03@gmail.com>
2025-03-18 16:08:16 +0530

    changes

--- pages/CH23BTECH11044.py
+++ pages/CH23BTECH11044.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Vardan Gupta - Ruthenium", layout="wide")
 
-# Include page.css for styling
-with open("assets/page.css") as f:
+# Load CSS
+with open("assets/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 st.title("Assignment: Vardan Gupta (Roll No. CH23BTECH11044)")
