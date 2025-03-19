1f43eee13c1fdf64aeab2b3259ce6230bd78acea
GuptaVardan <guptavardan03@gmail.com>
2025-03-19 16:38:03 +0530

    changes made

--- pages/CH23BTECH11034.py
+++ pages/CH23BTECH11034.py
@@ -78,7 +78,7 @@ with tab6:
     st.header("Electromagnetic Properties")
     st.write("- Electrical Conductivity: Low conductivity")
     st.write("- Magnetic Moment: Not significant")
-    st.write("- Emission Spectrum: only uv and x-ray region")
+    st.write("- Emission Spectrum: only UV and X-ray region")
 
 with tab7:
     st.header("Thermodynamic Properties")
